2879733f8409030fbf4b0587b0e2b70374be133c
Augusto <augstarachrafael@gmail.com>
2019-06-28 18:38:54 -0300

    validação do email do cadastro do usuario via javascript

--- Loja/validaUsuario.php
+++ Loja/validaUsuario.php
@@ -6,7 +6,7 @@ $result = mysqli_query($con,"SELECT senha FROM usuarios where nome='$nome'");
 $hash_senha = mysqli_fetch_assoc($result);
 $linhas = mysqli_num_rows($result);
 if($linhas <= 0 or !password_verify($_REQUEST['password'], $hash_senha['senha'] )){
-	echo 'falhou';
+	header('location: login.php?notFound=1')
 }
 else{
 	session_start();
@@ -14,11 +14,4 @@ else{
 	header('Location: index.php');
 }
 
-/*if ($linha > 0 AND password_verify($_REQUEST['password'], $linha['password'])){
-	session_start();
-	$_SESSION["name"] = $_REQUEST['name'];
-	header('location: index.php');*/
-//}else{
-//	header('location: login.php?notFound=1');
-//}
 ?>
