eb984c6eb80e85a83e2e4f744accfbbbcee297b8
MiguelRebelatto <Miguelgr199@hotmail.com>
2019-06-30 02:37:22 -0300

    remove arquivos

--- 1Loja_OLD/script.php
+++
@@ -1,9 +0,0 @@
-<!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Custom styles for this template -->
-  <link href="css/shop-homepage.css" rel="stylesheet">
